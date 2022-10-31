f19dab3bfcb8fa762f9722c3c45f5eaec1b15456
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-10-31 21:46:11 +0900

    完了

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,17 +1,27 @@
 #include "BasicShaderHeader.hlsli"
 
-[maxvertexcount(3)]
+[maxvertexcount(6)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION, 
-	inout TriangleStream< GSOutput > output
+	triangle VSOutput input[3] : SV_POSITION,
+	inout TriangleStream<GSOutput> output
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
+    for (uint i = 0; i < 3; i++)
+    {
+        GSOutput element;
+        element.svpos = input[i].svpos;
+        element.normal = input[i].normal;
         element.uv = input[i].uv;
-		output.Append(element);
-	}
+        output.Append(element);
+    }
+    output.RestartStrip();
+
+    for (uint i = 0; i < 3; i++)
+    {
+        GSOutput element;
+        element.svpos = input[i].svpos + float4(20.0f, 0.0f, 0.0f, 0.0f);
+        element.normal = input[i].normal;
+        element.uv = input[i].uv;
+        output.Append(element);
+    }
 }
